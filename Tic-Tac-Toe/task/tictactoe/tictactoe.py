69cde3f0959632a05b499d789dfa17504d251a9a
StrangeGreyCat <gxtrus@gmail.com>
2020-05-02 08:23:40 +0300

    added next move added game

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,6 +1,7 @@
 class TicTacToe:
+
     def __init__(self):
-        current_state = list(input("Enter cells: "))
+        current_state = list(input("Enter cells: ").replace('_', ' '))
         self.current_state = [current_state[0:3], current_state[3:6], current_state[6:9]]
 
     def show_table(self):
@@ -37,7 +38,37 @@ class TicTacToe:
         # no free space and nobody wins
         return "Draw"
 
+    def next_move(self):
+        digits = '0123456789'
+        coords = input("Enter the coordinates: ").split()
+        if len(coords) != 2 or coords[0] not in digits or coords[1] not in digits:
+            print("You should enter numbers!")
+            return False
+        self.coord_x, self.coord_y = int(coords[0]) - 1, 3 - int(coords[1])
+        if self.coord_x > 2 or self.coord_y > 2 or \
+                self.coord_x < 0 or self.coord_y < 0:
+            print("Coordinates should be from 1 to 3!")
+            return False
+        if self.check_coord():
+            self.current_state[self.coord_y][self.coord_x] = 'X'
+            return True
+        else:
+            print("This cell is occupied! Choose another one!")
+            return False
+
+    def check_coord(self):
+        if 'X' in self.current_state[self.coord_y][self.coord_x] or \
+                'O' in self.current_state[self.coord_y][self.coord_x]:
+            return False
+        return True
+
+    def game(self):
+        game.show_table()
+        check_ = True
+        while check_:
+            check_ = not game.next_move()
+        game.show_table()
+
 
 game = TicTacToe()
-game.show_table()
-print(game.check_game_state())
+game.game()
